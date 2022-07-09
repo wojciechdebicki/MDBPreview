f411bd8f54a5fe9baf914d72266d4a1e978bed09
Wojciech Debicki <wojciech.debicki@gmail.com>
2022-07-09 22:00:04 +0200

    Bottom navigation added

--- app/src/main/java/com/debicki/mdbpreview/ui/MainActivity.kt
+++ app/src/main/java/com/debicki/mdbpreview/ui/MainActivity.kt
@@ -6,10 +6,10 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.debicki.mdbpreview.R
 import com.debicki.mdbpreview.common.viewBinding
 import com.debicki.mdbpreview.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -26,11 +26,7 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        binding.bottomNavView.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
